d3c25504f055f215f3bdafbb2aea9bdf3ed573f2
Chirag Kumar <ck98738687@gmail.com>
2021-09-22 13:20:38 +0530

    initial commit

---
+++ src/main.js
@@ -0,0 +1,28 @@
+var initialPrice=document.querySelector('#initial-price');
+var quantityStocks=document.querySelector('#quantity-stocks');
+var currentPrice=document.querySelector('#current-price');
+var calculateBtn=document.querySelector('#calculate-btn')
+var message=document.querySelector('#message');
+
+function calculate(){
+    ipValue=initialPrice.value;
+    qsValue=quantityStocks.value;
+    cpValue=currentPrice.value;
+
+    if(ipValue>cpValue){
+        var loss=(ipValue-cpValue)*qsValue;
+        var lossPercentage=((loss/ipValue)*100).toFixed(2);
+        message.innerHTML=`Hey, the loss is ${loss} and the percent is ${lossPercentage}%`
+    }else if(ipValue<cpValue){
+        var profit=(cpValue-ipValue)*qsValue;
+        var profitPercentage=((profit/ipValue)*100).toFixed(2);
+
+        message.innerHTML=`Hey, the profit is ${profit} and the percent is ${profitPercentage}%`
+    }
+    else{
+        message.innerHTML=`No pain no gain and no gain no pain`
+    }
+
+
+}
+calculateBtn.addEventListener('click',calculate);
